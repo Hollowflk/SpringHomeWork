c06bc879977e6feeb1710d60bcfc94e78849e661
hollowflk <hollowflk@gmail.com>
2022-06-26 14:48:45 +0300

    Изменил application.yml

--- src/main/java/com/Neostudy/SpringHomeWork/Service/BookService.java
+++ src/main/java/com/Neostudy/SpringHomeWork/Service/BookService.java
@@ -1,7 +1,10 @@
 package com.Neostudy.SpringHomeWork.Service;
 
+import com.Neostudy.SpringHomeWork.Entities.Book;
 import com.Neostudy.SpringHomeWork.Repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -10,5 +13,9 @@ public class BookService {
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAll(){
+        return bookRepository.findAll();
     }
 }
